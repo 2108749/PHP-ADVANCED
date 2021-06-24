e80cb9125e7d2819116d8ca3e58cb2004b428efa
Diego <diegoblom01@gmail.com>
2021-06-24 12:52:20 +0200

    opdrahct

--- opdracht/advanced1.php
+++ opdracht/advanced1.php
@@ -1,45 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Advanced</title>
 </head>
-
 <body>
-    </select>
-    Achtergrond kleur;
-    <br>
-    <form action="advanced2.php" name="tekstkleur" method="post">
-        <select name="achtergrondkleur">
-            <?php
-            $kleuren = array("red", "blue", "green", "black", "brown");
-            foreach ($kleuren as $value) {
-                echo "<option>$value</option>";
-            }
-            ?>
-        </select>
-        <br>
-        Tekstkleur;
-        <br>
-        <select name="tekstkleur">
-            <?php
-            foreach ($kleuren as $value) {
-                echo "<option>$value</option>";
-            }
-            ?>
-        </select>
-        <button type="submit">Submit</button>
-    </form>
-    <br>
-
-
-
-
+Achtergrondkleur;
+<br>
+<select name="achtergrondkleur">
+<?php
+$kleuren = array("red", "blue", "green", "black", "brown");
+foreach ($kleuren as $value) {
+echo "<option>$value</option>";
+ }
+?>
+</select>
+<br>
+ Tekstkleur;
+<br>
+<select name="tekstkleur">
+<?php
+foreach ($kleuren as $value) {
+echo "<option>$value</option>";
+ }
+?>
+</select>
+<br>
 </body>
-
+    
 </body>
-
 </html>
